3dd78efa2169e23eb5dff6b415370551ebc2c66c
Bonifase <orwabonifase@gmail.com>
2025-03-01 23:12:41 +0300

    Add course form

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
 
 class CoursesPage extends Component {
+    state = {
+        course: {
+            title: ""
+        }
+    };
+
+    handleCourseChange = event => {
+        event.preventDefault();
+        const course = {...this.state.course, title: event.target.value};
+        this.setState({ course });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        console.log(this.state.course);
+    };
     render() {
         return (
-            <h2>
-                Courses
-            </h2>
+            <form onSubmit={this.handleSubmit}>
+                <h2>Courses</h2>
+                <h3>Add Course</h3>
+                <input type="text" name="course" value={this.state.course.title} onChange={this.handleCourseChange} />
+                <input type="submit" value="Save"/>
+            </form>
+
         );
     }
 }
